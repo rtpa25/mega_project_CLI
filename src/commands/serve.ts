18b80c2ff6f0df508098dab6439d3067a6950cbf
Ronit Panda <pandaronit25@gmail.com>
2021-11-12 12:39:23 +0530

    completed the pacakge

--- src/commands/serve.ts
+++ src/commands/serve.ts
@@ -1,16 +1,14 @@
 /** @format */
 
+import path from 'path';
 import { Command } from 'commander';
 import { serve } from 'local-api';
-import path from 'path';
 
-//will be always false on our system and will be always true on user's machine
-const isProd = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === 'production';
 
-//all user provided values are given as a string
 export const serveCommand = new Command()
   .command('serve [filename]')
-  .description('Open file for editing')
+  .description('Open a file for editing')
   .option('-p, --port <number>', 'port to run server on', '4005')
   .action(async (filename = 'notebook.js', options: { port: string }) => {
     try {
@@ -19,21 +17,17 @@ export const serveCommand = new Command()
         parseInt(options.port),
         path.basename(filename),
         dir,
-        !isProd
+        !isProduction
       );
       console.log(
         `Opened ${filename}. Navigate to http://localhost:${options.port} to edit the file.`
       );
-    } catch (error: any) {
-      //SAME PORT ERROR
-      if (error.code === 'EADDRINUSE') {
-        console.log(
-          'Port is in use. Try running on a different port by "node index.js serve --port=<another_port>".'
-        );
+    } catch (err: any) {
+      if (err.code === 'EADDRINUSE') {
+        console.error('Port is in use. Try running on a different port.');
       } else {
-        console.log('Here is the problem', error.message);
+        console.log('Heres the problem', err.message);
       }
-      //exit the programm on unsuccessful operation
       process.exit(1);
     }
   });
